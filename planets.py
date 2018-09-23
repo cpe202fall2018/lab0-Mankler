1cdff76accad239eff828f5ee6f18441c56f91e7
Matthew Reynolds <mreyno29@calpoly.edu>
2018-09-22 17:51:37 -0700

    Sat. 9/22/18 - Matthew Reynolds

--- planets.py
+++ planets.py
@@ -1,11 +1,9 @@
 def weight_on_planets():
    # write your code here
-   print("What do you weigh on earth?", end =' ')
-   weight = input()
-   print("On Mars you would weigh " + .38*int(weight) + " pounds")
+   weight = input("What do you weigh on earth? ")
+   marsWeight = int(weight) *.38
+   jupWeight = (marsWeight/.38)*2.34
+   print(f"\nOn Mars you would weigh {marsWeight} pounds.\nOn Jupiter you would weigh {jupWeight} pounds."  )
 
-   
-   
-   
 if __name__ == '__main__':
    weight_on_planets()
